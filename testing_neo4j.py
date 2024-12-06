545a2def1ae93107298abba092ed03846af2c8c1
Erika Meredith <erika@poap.io>
2024-12-06 13:31:48 -0500

    threading, do_merge implementations

--- testing_neo4j.py
+++ testing_neo4j.py
@@ -4,17 +4,18 @@ import subprocess
 import pydantic
 from typing import List
 import openai 
-import datetime
+from datetime import datetime
 import json
 from typing import Dict
 import random
 import threading
 import os
 import neo4j
+import datetime
 from copy import deepcopy
 #lock = threading.Lock()
 
-Researchathon_api_key='/'
+Researchathon_api_key=''
 
 # Setup OpenAI stuff
 
@@ -59,7 +60,7 @@ class Argument_(pydantic.BaseModel):
     conclusion:Statement
 
 def make_argument(statement="humans cause climate change"):
-  Argument_maker_sys_inst= "Take this proposition and create a supporting argument for it where the proposition given is the conclusion. Statements are the premises of the argument, they must be reasonable atomic statements where the meaning is clear and the statement is not a compound sentence. Avoid creating statements that could be broken down into separate sentences, for example instead of creating a statement like ' x is caused by a,b,c' create separate statements 'x is caused is caused by a', 'x is caused by b' , 'x is caused by c' instead.Supporting rules connect supporting statements (premises) to the conclusion. Please provide you response according to the structure provided."
+  Argument_maker_sys_inst= "Take this proposition and create a supporting argument for it where the proposition given is the conclusion. Statements are the premises of the argument, they must be reasonable atomic statements where the meaning is clear and the statement is not a compound sentence. Avoid creating statements that could be broken down into separate sentences, for example instead of creating a statement like ' x is caused by a,b,c' create separate statements 'x is caused is caused by a', 'x is caused by b' , 'x is caused by c' instead. Supporting rules connect supporting statements (premises) to the conclusion. Please provide you response according to the structure provided."
   response=call_completions_structured(system_instructions=Argument_maker_sys_inst,prompt=statement,pylance_structure=Argument_)
   if isinstance(response, str):
     dict=json.loads(response)
@@ -83,6 +84,7 @@ def create_supporting_statement( _text):
         nodes = session.run(q1, _text=_text)
         add_similarity_edges(target_node=_text)
         find_duplicates(0.5,_text)
+        # add_source(_text, session)
 
 
 
@@ -90,6 +92,14 @@ def add_edge(_supporting_statement, _conclusion):
    with driver.session() as session:
        q2 = "MATCH (s1:Statement {text: $_supporting_statement}) MATCH (s2:Statement {text: $_conclusion}) CREATE (s1)-[:supports]->(s2)"
        nodes = session.run(q2, _supporting_statement = _supporting_statement, _conclusion = _conclusion)
+
+def clean_db():
+    """clear database contents"""
+    with driver.session() as session:
+        query = """
+            MATCH (n) DETACH DELETE n;
+        """
+        session.run(query)
 
 # Decompose an argument into successive arguments until the specified depth level is achieved
 
@@ -112,7 +122,6 @@ def decompose_argument(statement="", depth=2):
         for supporting_statement in seed["supporting_statements"]:
             decompose_argument(supporting_statement.get("statement"),depth)
 
-
 # decompose_argument("humans are not solely responsible for climate change.", 3)
 
 # create new edges between all nodes in graph with weights calculated by sorensen dice similarity between nodes - ref: https://neo4j.com/labs/apoc/4.3/overview/apoc.text/apoc.text.sorensenDiceSimilarity/
@@ -132,8 +141,8 @@ def add_similarity_edges(target_node):
         session.run(query, target_node=target_node)
 
 # IN PROGRESS - identify edges with sd similarity above a certain threshold
-statement1="Ultraprocessed foods cause obesity."
-statement2="Ultraprocessed foods cause obesity."
+statement1="God exists."
+statement2="God does not exist."
 def campare_meanings(node1, node2):
   class Statement(pydantic.BaseModel):
     evaluation: bool
@@ -146,6 +155,37 @@ def campare_meanings(node1, node2):
 #   print(campare_meanings(statement1,statement2)["evaluation"],end=" ")
 
 
+def do_merge(node, session):
+    text_1 = node["target"]['text']
+    text_2 = node["b"]['text']
+    if campare_meanings(text_1,text_2)["evaluation"]:
+        query_merge = """
+
+            MATCH (s1:Statement {text: $text_1}), (s2:Statement {text: $text_2})
+            WITH s1, s2
+            MATCH (s2)-[r:supports]->(other)
+            MERGE (s1)-[r_new:supports]->(other) 
+            SET r_new += properties(r)
+
+            WITH s1, s2
+            MATCH (other)-[r:supports]->(s2)
+            MERGE (other)-[r_new:supports]->(s1)
+            SET r_new += properties(r)
+
+            DETACH DELETE s2
+            RETURN s1;
+        
+        """
+        merge_nodes = session.run(query_merge, text_1 = text_1, text_2 = text_2)
+        print(text_1, end=' ', flush=True)
+        print("merged with", end=' ', flush=True)
+        print(text_2, end=' ', flush=True)
+    print(".", end=' ', flush=True)
+    
+
+
+
+
 def find_duplicates(threshold,target_node_text):    
     query = """
     MATCH (target:Statement {text: $target_node_text})
@@ -156,40 +196,66 @@ def find_duplicates(threshold,target_node_text):
 
     with driver.session() as session:
       nodes = session.run(query, threshold=threshold, target_node_text=target_node_text)
-      candidates=[]
+      threads = [] 
       for node in nodes:
-        text_1 = node["target"]['text']
-        text_2 = node["b"]['text']
-        if campare_meanings(text_1,text_2)["evaluation"]:
-                candidates.append(text_2)
-                query_merge = """
+        thread=threading.Thread(target=do_merge,args=(node, session))
+        threads.append(thread)
+      for thread in threads:
+        thread.start()
+      for thread in threads:
+        thread.join()
+        # text_1 = node["target"]['text']
+        # text_2 = node["b"]['text']
+        # if campare_meanings(text_1,text_2)["evaluation"]:
+        #         candidates.append(text_2)
+        #         query_merge = """
 
-                    MATCH (s1:Statement {text: $text_1}), (s2:Statement {text: $text_2})
-                    WITH s1, s2
-                    MATCH (s2)-[r:supports]->(other)
-                    MERGE (s1)-[r_new:supports]->(other) 
-                    SET r_new += properties(r)
+        #             MATCH (s1:Statement {text: $text_1}), (s2:Statement {text: $text_2})
+        #             WITH s1, s2
+        #             MATCH (s2)-[r:supports]->(other)
+        #             MERGE (s1)-[r_new:supports]->(other) 
+        #             SET r_new += properties(r)
 
-                    WITH s1, s2
-                    MATCH (other)-[r:supports]->(s2)
-                    MERGE (other)-[r_new:supports]->(s1)
-                    SET r_new += properties(r)
+        #             WITH s1, s2
+        #             MATCH (other)-[r:supports]->(s2)
+        #             MERGE (other)-[r_new:supports]->(s1)
+        #             SET r_new += properties(r)
 
-                    DETACH DELETE s2
-                    RETURN s1;
+        #             DETACH DELETE s2
+        #             RETURN s1;
                 
-                """
-                merge_nodes = session.run(query_merge, text_1 = text_1, text_2 = text_2)
-                print(text_1)
-                print("merged with")
-                print(text_2)
+        #         """
+        #         merge_nodes = session.run(query_merge, text_1 = text_1, text_2 = text_2)
+        #         print(text_1)
+        #         print("merged with")
+        #         print(text_2)
                 # print("merged nodes with statements: {text_1}, {text_2}")
 
         # print(f"found {len(candidates)} candidates")
         # print (candidates)
-      
-   
-print(decompose_argument(statement1, 3))
+
+# def add_source(text, session):
+#     class Source(pydantic.BaseModel):
+#         link: str
+  
+#     system_inst="For the given claim, please find a real, reputable source that supports it and provide an active link to it. Please make sure the URL does not lead to a 404 page."
+#     response=call_completions_structured(system_instructions=system_inst,prompt=(text),pylance_structure=Source)
+#     # print(type(response))
+#     link = response["link"]
+#     query = """
+#         MATCH (s1:Statement {text: $text})
+#         SET s1.source = $link 
+#     """
+#     session.run(query, text = text, link = link)
+    
+
+
+clean_db()
+start = datetime.datetime.now()
+decompose_argument(statement1, 2)
+print(datetime.datetime.now() - start)
+# decompose_argument(statement2, 2)
+# print(datetime.datetime.now() - start)
 #print (find_duplicates(0.5,"Reduced satiety can lead to overeating."))
         
   
